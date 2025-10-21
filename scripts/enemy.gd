66c776e960773da2a6e2393fdf8a4b314869ed29
Naked-Snake04 <joKer041200saV@gmail.com>
2025-10-21 20:01:22 +0300

    feat #19. Враги видят пропасть и разворачиваются
    Мне не нравится, что оно реализовано через rayCast, но пока работает, пускай будет

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,8 +6,9 @@ const SPEED = 400
 var direction = 1
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_floor: RayCast2D = $RayCastFloor
 
-var hp = 4
+@export var hp = 4
 
 func _ready() -> void:
 	## Добавляем в группу врагов, для подсчёта их на уровне
@@ -21,6 +22,9 @@ func _physics_process(delta: float) -> void:
 		direction = -1
 	if ray_cast_left.is_colliding():
 		direction = 1
+	
+	if !ray_cast_floor.is_colliding() and is_on_floor():
+		direction = -direction
 	
 	velocity.x = direction * SPEED
 	
